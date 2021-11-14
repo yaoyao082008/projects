7126ea9673ede22d8be4a9aae8c6cbad9dd1fe8b
Quanren Xiong <Quanren.Xiong@spok.com>
2021-11-14 11:34:05 -0500

    Fibbonacci Number(Recursion)

---
+++ FibbonacciNumber(Recursion).py
@@ -0,0 +1,13 @@
+def FN(n):
+    if n ==1:
+        return 1
+    if n==0:
+        return 0
+        
+    fnMinus2=FN(n-2)
+    fnMinus1=FN(n-1)
+    return fnMinus2+fnMinus1
+    
+
+k=FN(5)
+print(k)
