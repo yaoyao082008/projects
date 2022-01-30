14df788b46b2d4611c5b3f5e658d90feb0bceefb
Quanren Xiong <Quanren.Xiong@spok.com>
2022-01-29 19:47:03 -0500

    Final Ver

--- MissingNumbers.py
+++ MissingNumbers.py
@@ -1,33 +1,28 @@
 
-Array=[5,8,10,15]
+Array=[5,6]
 LengthOfArray=len(Array)-1
-target=11
-run=True
-totalMissingNumbers= Array[LengthOfArray]-Array[0]-LengthOfArray
-if target>Array[0]:
-    target=target-Array[0]+1
-elif target<Array[0]:
-    print(target)
-    run=False
-
+target=5
+before=Array[0]-1
+totalMissingNumbers= Array[LengthOfArray]-Array[0]-LengthOfArray+before
+MissingNumbersHalf=0
+starting=0
+ending=len(Array)-1
+splittingPoint=0
 if target>totalMissingNumbers:
     print(Array[LengthOfArray]+(target-totalMissingNumbers))
-elif run==True:
-    MissingNumbersHalf=0
-    NthmissingNumber=0
-    starting=0
-    ending=len(Array)-1
-    while True:
+elif target<Array[0]:
+    print(target)
+else:
+    target=target-Array[0]+1
+    while Array[splittingPoint+1]-Array[splittingPoint]<=target:
         splittingPoint=(starting+ending)//2
         MissingNumbersHalf=Array[splittingPoint]-Array[starting]-(splittingPoint-starting)
         if MissingNumbersHalf<target:
             target=target-MissingNumbersHalf
-            if Array[splittingPoint+1]-Array[splittingPoint]>target:
-                print(Array[splittingPoint]+target)
-                break
             starting=splittingPoint
         elif MissingNumbersHalf>=target:
             ending=splittingPoint
+    print(Array[splittingPoint]+target)
 
 
         
